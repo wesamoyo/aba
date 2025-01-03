1e8e5b256156d5a7f9b0cad99b28b73b29a48a49
Wesamoyo Louis <152051064+wesamoyo@users.noreply.github.com>
2025-01-03 23:50:44 +0300

    Update llm.ts

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -2,16 +2,16 @@ import { ModelProvider } from '@/libs/agent-runtime';
 import { genUserLLMConfig } from '@/utils/genUserLLMConfig';
 
 export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
+  groq: {
+    enabled: true,
+  },
   ollama: {
     enabled: true,
     fetchOnClient: true,
   },
-  openai: {
-    enabled: true,
-  },
 });
 
-export const DEFAULT_MODEL = 'gpt-4o-mini';
+export const DEFAULT_MODEL = 'llama-3.2-90b-vision-preview';
 export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
 
-export const DEFAULT_PROVIDER = ModelProvider.OpenAI;
+export const DEFAULT_PROVIDER = ModelProvider.Groq;
